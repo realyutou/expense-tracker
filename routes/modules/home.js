e85e7f8b6f18763971c6dce7204a5ab4d57f4f03
realyutou <ssps9430@gmail.com>
2023-07-19 10:45:11 +0800

    feat: modify home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,29 @@
 // Include packages and define related variables
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
+const Record = require('../../models/record')
 
 // Set routes
 router.get('/', (req, res) => {
-  res.render('index')
+  let totalAmount = 0
+  Record.find()
+    .lean()
+    .then(records => {
+      for (let i = 0; i < records.length; i++) {
+        totalAmount += records[i].amount
+        records[i].date = records[i].date.toDateString()
+      }
+      recordList = records
+      return records
+    })
+    .then(records => {
+      Category.find()
+        .lean()
+        .then(category => {
+          res.render('index', { category, records, totalAmount })
+        })
+    })
 })
 
 // 匯出 home 路由模組
