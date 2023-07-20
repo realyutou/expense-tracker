6fbfb946ad56370c6c51028dfd2e37290871332a
realyutou <ssps9430@gmail.com>
2023-07-20 20:06:29 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,14 @@
 // Include packages and define related variables
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 module.exports = app => {
   // 初始化 Passport 模組
   app.use(passport.initialize())
   app.use(passport.session())
+  
   // 設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
@@ -14,13 +16,17 @@ module.exports = app => {
         if (!user) {
           return done(null, false, req.flash('warning_msg', '此 Email 尚未註冊！'))
         }
-        if (user.password !== password) {
-          return done(null, false, req.flash('warning_msg', 'Email 或密碼錯誤！'))
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, req.flash('warning_msg', 'Email 或密碼錯誤！'))
+            }
+            return done(null, user)
+          })
       })
       .catch(error => done(error, false))
   }))
+
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
