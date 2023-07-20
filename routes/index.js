4274b9a23256b2a30ee181c8c6abf85cbd8e621b
realyutou <ssps9430@gmail.com>
2023-07-20 22:17:54 +0800

    feat: add facebook login

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 // Include packages and define related variables
 const express = require('express')
 const router = express.Router()
+
 const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth')
+
 
 // 將網址結構符合 '/records' 的 request 傳入 records 模組
 router.use('/records', authenticator, records)
 
 // 將網址結構符合 '/users' 的 request 傳入 users 模組
 router.use('/users', users)
+
+// 將網址結構符合 '/auth' 的 request 傳入auth模組
+router.use('/auth', auth)
 
 // 將網址結構符合 '/' 的 request 傳入 home 模組
 router.use('/', authenticator, home)
